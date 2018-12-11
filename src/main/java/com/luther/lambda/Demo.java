47b86162cb6d0c91a79704e793313c44c08bc821
luther <maseratipp@gmail.com>
2018-12-11 19:57:48 +0800

    Lambda demo

---
+++ src/main/java/com/luther/lambda/Demo.java
@@ -0,0 +1,15 @@
+package com.luther.lambda;
+
+/**
+ * @Author: cpzh（不器）
+ * @CreateTime: 2018/12/11 19:49
+ * @UpdateTime: 2018/12/11 19:49
+ * @Version: 1.0.0
+ * @PackageName: com.luther.lambda
+ * TODO:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("This My Demo");
+    }
+}
